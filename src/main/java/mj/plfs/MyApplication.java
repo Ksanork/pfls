6ec8f682019cb940054f340c011dfb34b01ff86e
Ksanork <miki11@poczta.onet.pl>
2017-10-06 16:11:53 +0200

    Try deploy on Heroku

--- src/main/java/mj/plfs/MyApplication.java
+++ src/main/java/mj/plfs/MyApplication.java
@@ -17,7 +17,7 @@ public class MyApplication extends Application{
         return h;
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
 
     }
 }
