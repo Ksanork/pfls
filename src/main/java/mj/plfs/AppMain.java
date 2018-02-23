dd3410f25d1b23577764b09b9b90f8c957840f8a
Ksanork <miki11@poczta.onet.pl>
2018-02-23 16:09:09 +0100

    Test old server

--- src/main/java/mj/plfs/AppMain.java
+++ src/main/java/mj/plfs/AppMain.java
@@ -1,7 +1,11 @@
 package mj.plfs;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.sun.grizzly.http.SelectorThread;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -9,50 +13,51 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class AppMain {
 
     public static void main(String[] args) throws IOException {
-//        final String baseUri = "http://localhost:" + (System.getenv("PORT") != null ? System.getenv("PORT") : "9998")+"/";
-//        final Map<String, String> initParams = new HashMap<String, String>();
-////        initParams.put("com.sun.jersey.config.property.packages", "mj.plfs.net");
+        final String baseUri = "http://localhost:" + (System.getenv("PORT") != null ? System.getenv("PORT") : "9998")+"/";
+        final Map<String, String> initParams = new HashMap<String, String>();
+        initParams.put("com.sun.jersey.config.property.packages", "mj.plfs.net");
+
+        System.out.println("Starting grizzly...");
+        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
+        System.out.println(String.format("Jersey started with %s.", baseUri));
+
+
+
+
+
+
+//        String webappDirLocation = "src/main/webapp/";
 //
-//        System.out.println("Starting grizzly...");
-//        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
-//        System.out.println(String.format("Jersey started with %s.", baseUri));
-
-
-
-
-
-
-        String webappDirLocation = "src/main/webapp/";
-
-        // The port that we should run on can be set into an environment variable
-        // Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
-
-        Server server = new Server(Integer.valueOf(webPort));
-        WebAppContext root = new WebAppContext();
-
-        root.setContextPath("/");
-        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        root.setResourceBase(webappDirLocation);
-
-        // Parent loader priority is a class loader setting that Jetty accepts.
-        // By default Jetty will behave like most web containers in that it will
-        // allow your application to replace non-server libraries that are part of the
-        // container. Setting parent loader priority to true changes this behavior.
-        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
-        root.setParentLoaderPriority(true);
-
-        server.setHandler(root);
-
-        try {
-            server.start();
-            System.out.println("Start on http://localhost:8080");
-            server.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        // The port that we should run on can be set into an environment variable
+//        // Look for that variable and default to 8080 if it isn't there.
+//        String webPort = System.getenv("PORT");
+//        if (webPort == null || webPort.isEmpty()) {
+//            webPort = "8080";
+//        }
+//
+//        Server server = new Server(Integer.valueOf(webPort));
+//        WebAppContext root = new WebAppContext();
+//
+//        root.setContextPath("/");
+//        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+//        root.setResourceBase(webappDirLocation);
+////        root.set
+//
+//        // Parent loader priority is a class loader setting that Jetty accepts.
+//        // By default Jetty will behave like most web containers in that it will
+//        // allow your application to replace non-server libraries that are part of the
+//        // container. Setting parent loader priority to true changes this behavior.
+//        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+//        root.setParentLoaderPriority(true);
+//
+//        server.setHandler(root);
+//
+//        try {
+//            server.start();
+//            System.out.println("Start on http://localhost:8080");
+//            server.join();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
